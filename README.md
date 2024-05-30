fa4c4d9cdea1f766a5d33ae6bcbc7fcb2a589663
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2024-05-30 13:50:29 -0400

    Update README, Add Makejail, Add scripts, Add files

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# wireguard
-Fast, modern and secure VPN Tunnel.
+# WireGuard
+
+WireGuard is a communication protocol and free and open-source software that implements encrypted virtual private networks (VPNs), and was designed with the goals of ease of use, high speed performance, and low attack surface. It aims to be smaller and better performing than IPsec and OpenVPN, two common tunneling protocols. The WireGuard protocol passes traffic over UDP.
+
+wikipedia.org/wiki/WireGuard
+
+![wireguard logo](https://upload.wikimedia.org/wikipedia/commons/9/98/Logo_of_WireGuard.svg)
+
+## How to use this Makejail
+
+### Requirements
+
+Before continuing, we need to load the `if_wg(4)` driver:
+
+```sh
+kldload if_wg
+```
+
+And add it to `loader.conf(5)` to load it at boot:
+
+**/boot/loader.conf**:
+
+```
+if_wg_load="YES"
+```
+
+### Basic usage
+
+```sh
+mkdir -p .volumes/wg-etc
+appjail makejail \
+	-j wireguard \
+	-f gh+AppJail-makejails/wireguard
+	-o virtualnet=":<random> default" \
+	-o nat \
+	-o expose="51820 proto:udp" \
+	-o volume="$PWD/.volumes/wg-etc wireguard-etc <volumefs>" \
+	-V WG_ENDPOINT=192.168.1.112 \
+	-V WG_PERSISTENTKEEPALIVE=25
+```
+
+### List all peers
+
+
+
+```sh
+appjail run \
+	-s ls-peers \
+	wireguard
+```
+
+### Show a given peer
+
+```sh
+appjail run \
+	-s show-peer \
+	-V WG_PEERID=2 \
+	wireguard
+```
+
+#### Environment
+
+* `WG_PEERID` (mandatory): Peer ID.
+* `WG_HIDE_QRCODE` (optional): Set this environment variable to not display the QR code.
+
+### Search a peer
+
+```sh
+appjail run \
+	-s search-peer \
+	-V WG_PATTERN="something" \
+	wireguard
+```
+
+#### Environment
+
+* `WG_PATTERN` (default: `.+`): Pattern to find matches.
+
+### Arguments
+
+* `wg_tag` (default: `13.3`): See [#tags](#tags).
+* `wg_srvconf` (default: `files/server-template.conf`): Template for the server.
+* `wg_peerconf` (default: `files/peer-template.conf`): Template for the peer.
+
+### Environment
+
+* `WG_ENDPOINT` (optional): See `Endpoint` in `wg(8)`. If undefined, an attempt is made to obtain an IPv4 address from the default interface, which of course may not make sense, so it is advisable to set this environment variable explicitly.
+* `WG_DNS` (optional): See `DNS` in `wg-quick(8)`. DNS servers for each peer. If undefined, the `resolv.conf(5)` file is used and if it does not exist, OpenDNS nameservers will be used.
+* `WG_PERSISTENTKEEPALIVE` (optional): See `PersistentKeepalive` in `wg(8)`.
+* `WG_ALLOWEDIPS` (default: `0.0.0.0/0`): See `AllowedIPs` in `wg(8)`.
+* `WG_SUBNET` (default: `192.168.6.0`): A 24-bit long network address. The last octet is ignored, but 0 is used as a convention.
+* `WG_PORT` (default: `51820`): See `ListenPort` in `wg(8)`.
+* `WG_PEERS` (default: `1`): Total number of pairs to create. A valid number is between 1 and 253. Note that 2 is the first peer ID assigned, 1 is reserved by the server.
+* `WG_MTU` (optional): See `MTU` in `wg-quick(8)`. Set this environment variable to `none` to leave it unspecified.
+* `WG_FORCE_UPDATE` (optional): The configuration files are not regenerated if the environment variables `WG_ENDPOINT`, `WG_DNS`, `WG_PERSISTENTKEEPALIVE`, `WG_ALLOWEDIPS`, `WG_SUBNET`, `WG_PORT`, `WG_PEERS` and `WG_MTU` are not modified, but as you can see, peer environment variables are not taken into account, so this environment variable is used to forcibly regenerate the configuration files.
+* `WG_PEER<peer-id>_ENDPOINT`: Set a different `Endpoint` than global.
+* `WG_PEER<peer-id>_DNS`: Set a different `DNS` than global. Set this environment variable to `none` to leave it unspecified.
+* `WG_PEER<peer-id>_PERSISTENTKEEPALIVE`: Set a different `PersistentKeepalive` than global. Set this environment variable to `none` to leave it unspecified.
+* `WG_PEER<peer-id>_ALLOWEDIPS`: Set a different `Endpoint` than global.
+* `WG_PEER<peer-id>_PORT`: Set a different `ListenPort` than global. Set this environment variable to `none` to leave it unspecified.
+* `WG_PEER<peer-id>_MTU`: Set a different `MTU` than global. Set this environment variable to `none` to leave it unspecified.
+* `WG_PEER<peer-id>_NOTE`: A simple note, useful when searching.
+
+## Tag
+
+| Tag    | Arch    | Version        | Type   |
+| ------ | ------- | -------------- | ------ |
+| `13.3` | `amd64` | `13.3-RELEASE` | `thin` |
+| `14.0` | `amd64` | `14.0-RELEASE` | `thin` |
---
+++ scripts/configure-wireguard.sh
@@ -0,0 +1,209 @@
+#!/bin/sh
+
+# Environment:
+# - WG_ENDPOINT (optional)
+# - WG_DNS (optional)
+# - WG_PERSISTENTKEEPALIVE (optional)
+# - WG_ALLOWEDIPS (0.0.0.0/0)
+# - WG_SUBNET (192.168.6.0)
+# - WG_PORT (51820)
+# - WG_PEERS (1)
+# - WG_MTU (optional)
+# - WG_FORCE_UPDATE (optional)
+# - WG_PEER<peer-id>_{ENDPOINT|DNS|PERSISTENTKEEPALIVE|ALLOWEDIPS|PORT|MTU|NOTE} (optional)
+
+set -e
+
+umask 077
+
+. /scripts/lib.subr
+
+RESOLVCONF="/etc/resolv.conf"
+ETCDIR="/usr/local/etc/wireguard"
+SRVCONF="${ETCDIR}/wg0.conf"
+PEERDIR="${ETCDIR}/peers"
+DNS_NS1="208.67.222.222"
+DNS_NS2="208.67.220.220"
+WG_FALLBACK_DNS="${DNS_NS1},${DNS_NS2}"
+ENVFILE="${ETCDIR}/.env"
+
+if [ -z "${WG_ENDPOINT}" ]; then
+	default_interface=`get_default_interface`
+
+	if [ -z "${default_interface}" ]; then
+		err "Error getting default interface."
+		exit 1
+	fi
+
+	WG_ENDPOINT=`get_ip4_from_iface "${default_interface}"`
+
+	if [ -z "${WG_ENDPOINT}" ]; then
+		err "Failed to get default IPv4 address."
+		exit 1
+	fi
+fi
+
+if [ -z "${WG_DNS}" ]; then
+	if [ -f "${RESOLVCONF}" ]; then
+		WG_DNS=`get_dns_list "${RESOLVCONF}"`
+	fi
+
+	if [ -z "${WG_DNS}" ]; then
+		warn "Unable to retrieve a list of DNS nameservers, using the default ones: ${WG_FALLBACK_DNS}"
+
+		WG_DNS="${WG_FALLBACK_DNS}"
+	fi
+fi
+
+if [ -z "${WG_PEERS}" ]; then
+	WG_PEERS=1
+elif ! chk_number "${WG_PEERS}" || [ ${WG_PEERS} -lt 1 -o ${WG_PEERS} -gt 253 ]; then
+	err "Invalid total of peers: ${WG_PEERS}"
+	exit 1
+fi
+
+if [ -z "${WG_SUBNET}" ]; then
+	WG_SUBNET="192.168.6.0"
+fi
+
+if [ -z "${WG_PORT}" ]; then
+	WG_PORT="51820"
+fi
+
+if [ -z "${WG_ALLOWEDIPS}" ]; then
+	WG_ALLOWEDIPS="0.0.0.0/0"
+fi
+
+if ! chk_basic_ip4 "${WG_SUBNET}"; then
+	err "Invalid IPv4 address: ${WG_SUBNET}"
+	exit 1
+fi
+
+envfile=`mktemp -t wireguard`
+
+cat << EOF > "${envfile}"
+WG_ENDPOINT=${WG_ENDPOINT}
+WG_DNS=${WG_DNS}
+WG_PERSISTENTKEEPALIVE=${WG_PERSISTENTKEEPALIVE}
+WG_ALLOWEDIPS=${WG_ALLOWEDIPS}
+WG_SUBNET=${WG_SUBNET}
+WG_PORT=${WG_PORT}
+WG_PEERS=${WG_PEERS}
+WG_MTU=${WG_MTU}
+EOF
+
+if [ -z "${WG_FORCE_UPDATE}" ] && [ -f "${ENVFILE}" ]; then
+	a=`sha256 -q -- "${ENVFILE}"`
+	b=`sha256 -q -- "${envfile}"`
+
+	if [ "${a}" != "${b}" ]; then
+		warn "Environment variables changed, regenerating configuration files ..."
+	else
+		info "No changes were made to the environment variables, using the current ones."
+		exit 0
+	fi
+fi
+
+info "Configuring ..."
+
+WG_SUBNET=`printf "%s" "${WG_SUBNET}" | sed -Ee 's/\.[0-9]$//'`
+
+# server
+
+genkeys "${ETCDIR}"
+
+cp "${ETCDIR}/server-template.conf" "${SRVCONF}"
+
+cat << EOF >> "${SRVCONF}"
+[Interface]
+Address = ${WG_SUBNET}.1/32
+ListenPort = ${WG_PORT}
+PrivateKey = `getprivkey "${ETCDIR}"`
+EOF
+
+if [ -n "${WG_MTU}" ] && [ "${WG_MTU}" != "none" ]; then
+	echo "MTU = ${WG_MTU}" >> "${SRVCONF}"
+fi
+
+# peers
+
+count=0
+
+for peerid in `jot - 2 254`; do
+	peerdir="${PEERDIR}/${peerid}"
+	peer_address="${WG_SUBNET}.${peerid}"
+
+	# peer environment variables
+	peer_endpoint=`eval echo \\\$WG_PEER${peerid}_ENDPOINT`
+	peer_dns=`eval echo \\\$WG_PEER${peerid}_DNS`
+	peer_persistentkeepalive=`eval echo \\\$WG_PEER${peerid}_PERSISTENTKEEPALIVE`
+	peer_allowedips=`eval echo \\\$WG_PEER${peerid}_ALLOWEDIPS`
+	peer_port=`eval echo \\\$WG_PEER${peerid}_PORT`
+	peer_mtu=`eval echo \\\$WG_PEER${peerid}_MTU`
+	peer_note=`eval echo \\\$WG_PEER${peerid}_NOTE`
+
+	# default
+	peer_endpoint="${peer_endpoint:-${WG_ENDPOINT}}"
+	peer_dns="${peer_dns:-${WG_DNS}}"
+	peer_persistentkeepalive="${peer_persistentkeepalive:-${WG_PERSISTENTKEEPALIVE}}"
+	peer_allowedips="${peer_allowedips:-${WG_ALLOWEDIPS}}"
+	peer_port="${peer_port:-${WG_PORT}}"
+	peer_mtu="${peer_mtu:-${WG_MTU}}"
+
+	mkdir -p "${peerdir}"
+
+	genkeys "${peerdir}"
+
+	cat << EOF >> "${SRVCONF}"
+[Peer]
+AllowedIPs = ${peer_address}/32
+PresharedKey = `getpresharedkey "${peerdir}"`
+PublicKey = `getpubkey "${peerdir}"`
+EOF
+
+	peerconf="${peerdir}/wg.conf"
+
+	cp "${ETCDIR}/peer-template.conf" "${peerconf}"
+
+	cat << EOF >> "${peerconf}"
+[Interface]
+PrivateKey = `getprivkey "${peerdir}"`
+Address = ${peer_address}/32
+EOF
+
+	if [ "${peer_port}" != "none" ]; then
+		echo "ListenPort = ${peer_port}" >> "${peerconf}"
+	fi
+
+	if [ "${peer_dns}" != "none" ]; then
+		echo "DNS = ${peer_dns}" >> "${peerconf}"
+	fi
+
+	if [ -n "${peer_mtu}" ] && [ "${peer_mtu}" != "none" ]; then
+		echo "MTU = ${peer_mtu}" >> "${peerconf}"
+	fi
+
+	cat << EOF >> "${peerconf}"
+[Peer]
+PresharedKey = `getpresharedkey "${peerdir}"`
+PublicKey = `getpubkey "${ETCDIR}"`
+AllowedIPs = ${peer_allowedips}
+Endpoint = ${peer_endpoint}:${peer_port}
+EOF
+
+	if [ -n "${peer_persistentkeepalive}" ] && [ "${peer_persistentkeepalive}" != "none" ]; then
+		echo "PersistentKeepalive = ${peer_persistentkeepalive}" >> "${peerconf}"
+	fi
+
+	if [ -n "${peer_note}" ]; then
+		printf "%s\n" "${peer_note}" > "${peerdir}/.note"
+	fi
+
+	count=$((count+1))
+
+	if [ ${count} -ge ${WG_PEERS} ]; then
+		break
+	fi
+done
+
+mv "${envfile}" "${ENVFILE}"
